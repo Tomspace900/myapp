51db506244aebb4ccd163d2bef997534cff78109
Tomspace900 <tomspace.800@gmail.com>
2023-05-17 15:53:39 +0200

    vercel test

--- api/index.py
+++ api/index.py
@@ -4,9 +4,11 @@ app = Flask(__name__)
 
 stored_data = []
 
+
 @app.route("/")
 def hello():
-    return "Hello, world!"
+    return "Hello, world !"
+
 
 @app.route("/webhook", methods=["POST"])
 def handleWebhook():
@@ -14,9 +16,11 @@ def handleWebhook():
     stored_data.append(data)
     return "OK"
 
+
 @app.route("/data")
 def get_data():
     return jsonify(stored_data)
+
 
 if __name__ == "__main__":
     app.run()
